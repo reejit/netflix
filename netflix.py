ca08ed2d4567bd401d9a3776113c9b166a5dcdef
reejit <70316178+reejit@users.noreply.github.com>
2020-09-17 20:33:57 +0530

    Update netflix.py

--- netflix.py
+++ netflix.py
@@ -1,11 +1,5 @@
 # YOU NEED TO BE CONNECTED TO THE INTERNET VIA A GERMAN VPN
 # I USED WINDSCRIBE. IT's free and it's amazing.
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import requests
 import random
